88c15bee157248686513dea2074e07a19b4d2b70
Bryan <junhang.lee@ciceron.me>
2018-07-03 20:40:35 +0900

    Initial commit of Tissue

---
+++ noun/extractNoun.py
@@ -0,0 +1,35 @@
+import re
+import csv
+
+
+class ExtractNoun(object):
+    def __init__(self):
+        self.stopwords = []
+        self._loadStopwordList("stopwords.csv")
+
+    def _loadStopwordList(self, filename):
+        with open(filename, 'r') as f:
+            csvReader = csv.reader(f)
+            for row in csvReader:
+                self.stopwords.append(row[0])
+
+        self.stopwords.sort(key=len, reverse=True)
+
+    def extract(self, chunk):
+        regex = r'(.*?){}\Z'
+        extracted_word = ""
+        for word in self.stopwords:
+            extracted_obj = re.search(regex.format(word), chunk)
+            if extracted_obj is None:
+                continue
+
+            else:
+                extracted_word = extracted_obj.group(1)
+                break
+
+        return extracted_word
+
+if __name__ == "__main__":
+    obj = ExtractNoun()
+    word = obj.extract("늦잠은커녕")
+    print(word)
